b879bb9c4e901aac0b88ee25cbe9c54b4c667afb
Kavya Muppalla <kavyamuppalla@zipcoders-MBP-2.fios-router.home>
2019-06-01 17:56:39 -0400

    tic tac toe

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,99 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] matrix;
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        boolean b = false;
+        b = isDisagnol1('X');
+        b = b || isDiagnol2('X');
+        b = b || isColumn1('X');
+        b = b || isRow('X');
+        return b;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        boolean b = false;
+        b = isDisagnol1('O');
+        b = b || isDiagnol2('O');
+        b = b || isColumn1('O');
+        b = b || isRow('O');
+        return b;
     }
 
     public Boolean isTie() {
-        return null;
+
+        return !(isInFavorOfO() || isInFavorOfX());
     }
 
     public String getWinner() {
-        return null;
+        String winner = "";
+        if(isInFavorOfX()) {
+            winner = "X";
+        }
+        else if(isInFavorOfO()) {
+            winner = "O";
+        }
+
+        return winner;
+    }
+
+    public Boolean isDisagnol1(Character c) {
+        for(int i = 0 ; i < matrix.length;i++){
+            if(matrix[i][i] != c) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public Boolean isDiagnol2(Character c) {
+        for(int i = 0 ; i < matrix.length;i++){
+            for(int j = 0; j < matrix.length; j++) {
+               if(i+j == 2 && matrix[i][j] != c) {
+                   return false;
+               }
+
+                }
+
+            }
+
+        return true;
+    }
+    public Boolean isColumn1(Character c) {
+        for(int i = 0; i < matrix.length; i++) {
+            boolean isColumnMatching = true;
+            for (int j = 0; j < matrix.length; j++) {
+                if(matrix[j][i] != c) {
+                    isColumnMatching = false;
+                }
+            }
+            if (isColumnMatching) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public Boolean isRow(Character c) {
+        for(int i = 0; i < matrix.length; i++) {
+            boolean isRowMatching = true;
+            for (int j = 0; j < matrix.length; j++) {
+                if(matrix[i][j] != c) {
+                    isRowMatching = false;
+                }
+            }
+            if (isRowMatching) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
+
+
+
